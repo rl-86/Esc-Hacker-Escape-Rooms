a3e6b840aa5c41a06f41656a8dac6dbc93560533
rl-86 <rl86@live.se>
2023-11-10 14:36:31 +0100

    Added fade in animation to the mobile menu

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 
 // Variables
 const menuButton = document.querySelector(".mobileMenuButton");
-const menuClose = document.querySelector(".mobileMenuClose");
+const menuClose = document.querySelector(".mobileMenuCloseButton");
 
 
 // Listen for button click to change display property and prevent scrolling
 menuButton.addEventListener("click", function () {
-    document.getElementsByClassName("mobileMenuBackground")[0].style.display = "block";
+    document.getElementsByClassName("mobileMenuBackground")[0].style.display = "flex";
     document.documentElement.style.overflowY = "hidden";
 });
 
